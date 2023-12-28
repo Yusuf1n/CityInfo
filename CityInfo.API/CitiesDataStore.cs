998b752e3ae26f4f57377e521baf92ca11ce284b
Yusuf Naheem <Yusuf.Naheem@vercoglobal.com>
2023-12-28 20:41:51 +0000

    #3.15 - Adding each cities points of interest to a data store and adding its own model and controller

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -6,7 +6,7 @@ public class CitiesDataStore
 {
     public List<CityDto> Cities { get; set; }
 
-    public static  CitiesDataStore Current { get; } = new CitiesDataStore();
+    public static CitiesDataStore Current { get; } = new CitiesDataStore();
 
     public CitiesDataStore()
     {
@@ -17,19 +17,52 @@ public class CitiesDataStore
             {
                 Id = 1,
                 Name = "Stoke-on-Trent",
-                Description = "Residing town."
+                Description = "Residing town.",
+                PointsOfInterest = new List<PointOfInterestDto>()
+                {
+                    new PointOfInterestDto() {
+                        Id = 1,
+                        Name = "The Potteries Museum & Art Gallery",
+                        Description = "Displays locally made ceramics, decorative arts and a WWII Spitfire." },
+                    new PointOfInterestDto() {
+                        Id = 2,
+                        Name = "Wedgwood Museum",
+                        Description = "£10 million Wedgwood Museum visitor centre." }
+                }
             },
             new CityDto()
             {
                 Id = 2,
                 Name = "Chakswari",
-                Description = "Village home."
+                Description = "Village home.",
+                PointsOfInterest = new List<PointOfInterestDto>()
+                {
+                    new PointOfInterestDto() {
+                        Id = 3,
+                        Name = "Mangla Dam",
+                        Description = "The sixth-largest dam in the world." },
+                    new PointOfInterestDto() {
+                        Id = 4,
+                        Name = "Chakswari Grand Central Jamia Mosque",
+                        Description = "The main masjid in Chakswari." }
+                }
             },
             new CityDto()
             {
                 Id = 3,
                 Name = "Islamabad",
-                Description = "City home."
+                Description = "City home.",
+                PointsOfInterest = new List<PointOfInterestDto>()
+                {
+                    new PointOfInterestDto() {
+                        Id = 5,
+                        Name = "Faisal Mosque",
+                        Description = "The fifth-largest mosque in the world and the largest within South Asia, located on the foothills of Margalla Hills in Islamabad. It is named after the late King Faisal of Saudi Arabia." },
+                    new PointOfInterestDto() {
+                        Id = 6,
+                        Name = "Pakistan Monument",
+                        Description = "A national monument and heritage museum located on the western Shakarparian Hills in Islamabad, Pakistan." },
+                }
             },
         };
     }
