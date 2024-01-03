b57890b6d2315595a8b908ccbb51e31d786293a1
Yusuf Naheem <Yusuf.Naheem@vercoglobal.com>
2024-01-03 18:08:55 +0000

    #5.3 - Injecting and using a logger

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -8,6 +8,13 @@ namespace CityInfo.API.Controllers;
 [ApiController]
 public class PointsOfInterestController : ControllerBase
 {
+    private readonly ILogger<PointsOfInterestController> _logger;
+
+    public PointsOfInterestController(ILogger<PointsOfInterestController> logger)
+    {
+        _logger = logger;
+    }
+
     [HttpGet]
     public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
     {
@@ -15,6 +22,8 @@ public class PointsOfInterestController : ControllerBase
 
         if (city == null)
         {
+            _logger.LogInformation($"City with id {cityId} wasn't found when accessing points of interest");
+
             return NotFound();
         }
 
