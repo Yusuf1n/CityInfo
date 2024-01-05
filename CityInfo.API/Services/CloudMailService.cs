aebcd7a0690817072ede95c82a338f20cb549e9a
Yusuf Naheem <Yusuf.Naheem@vercoglobal.com>
2024-01-05 09:08:51 +0000

    #5.12 - Stored mail settings in appsettings and retrieved the values in both mail services

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -2,8 +2,14 @@
 
 public class CloudMailService : IMailService
 {
-    private string _mailTo = "admin@emailprovider.com";
-    private string _mailFrom = "noreply@emailprovider.com";
+    private readonly string _mailTo = string.Empty;
+    private readonly string _mailFrom = string.Empty;
+
+    public CloudMailService(IConfiguration configuration)
+    {
+        _mailTo = configuration["mailSettings:mailToAddress"];
+        _mailFrom = configuration["mailSettings:mailFromAddress"];
+    }
 
     public void Send(string subject, string message)
     {
