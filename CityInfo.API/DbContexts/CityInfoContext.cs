501fefd1a5af4976194260c25971c1ae436a3a6b
Yusuf Naheem <Yusuf.Naheem@vercoglobal.com>
2024-01-20 17:58:18 +0000

    #6.8 - Seeded the database with data

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -1,5 +1,7 @@
 ﻿using CityInfo.API.Entities;
+using CityInfo.API.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
 
 namespace CityInfo.API.DbContexts;
 
@@ -12,6 +14,67 @@ public class CityInfoContext : DbContext
     public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
     {
         
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<City>()
+            .HasData(
+                new City("Stoke-on-Trent")
+                {
+                    Id = 1,
+                    Description = "Residing town."
+                },
+                new City("Chakswari")
+                {
+                    Id = 2,
+                    Description = "Village home."
+                },
+                new City("Islamabad")
+                {
+                    Id = 3,
+                    Description = "City home.",
+                });
+
+        modelBuilder.Entity<PointOfInterest>()
+            .HasData(
+            new PointOfInterest("The Potteries Museum & Art Gallery") 
+            {
+                Id = 1,
+                CityId = 1,
+                Description = "Displays locally made ceramics, decorative arts and a WWII Spitfire."
+            },
+            new PointOfInterest("Wedgwood Museum") {
+                Id = 2,
+                CityId = 1,
+                Description = "£10 million Wedgwood Museum visitor centre."
+            },
+            new PointOfInterest("Mangla Dam")
+            {
+                Id = 3,
+                CityId = 2,
+                Description = "The sixth-largest dam in the world."
+            },
+            new PointOfInterest("Chakswari Grand Central Jamia Mosque")
+            {
+                Id = 4,
+                CityId = 2,
+                Description = "The main masjid in Chakswari."
+            },
+            new PointOfInterest("Faisal Mosque")
+            {
+                Id = 5,
+                CityId = 3,
+                Description = "The fifth-largest mosque in the world and the largest within South Asia, located on the foothills of Margalla Hills in Islamabad. It is named after the late King Faisal of Saudi Arabia."
+            },
+            new PointOfInterest("Pakistan Monument")
+            {
+                Id = 6,
+                CityId = 3,
+                Description = "A national monument and heritage museum located on the western Shakarparian Hills in Islamabad, Pakistan."
+            });
+
+        base.OnModelCreating(modelBuilder);
     }
 }
 
---
+++ CityInfo.API/Migrations/20240120175455_DataSeed.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityInfo.API.Migrations
+{
+    public partial class DataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "Residing town.", "Stoke-on-Trent" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 2, "Village home.", "Chakswari" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 3, "City home.", "Islamabad" });
+
+            migrationBuilder.InsertData(
+                table: "PointOfInterests",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Displays locally made ceramics, decorative arts and a WWII Spitfire.", "The Potteries Museum & Art Gallery" },
+                    { 2, 1, "£10 million Wedgwood Museum visitor centre.", "Wedgwood Museum" },
+                    { 3, 2, "The sixth-largest dam in the world.", "Mangla Dam" },
+                    { 4, 2, "The main masjid in Chakswari.", "Chakswari Grand Central Jamia Mosque" },
+                    { 5, 3, "The fifth-largest mosque in the world and the largest within South Asia, located on the foothills of Margalla Hills in Islamabad. It is named after the late King Faisal of Saudi Arabia.", "Faisal Mosque" },
+                    { 6, 3, "A national monument and heritage museum located on the western Shakarparian Hills in Islamabad, Pakistan.", "Pakistan Monument" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
