1c8e633cb7af6c0b558ef9eb281028206e127b6c
Yusuf Naheem <Yusuf.Naheem@vercoglobal.com>
2024-01-31 14:50:31 +0000

    Updated the table name to PointsOfInterest

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -9,7 +9,7 @@ public class CityInfoContext : DbContext
 {
     public DbSet<City> Cities { get; set; } = null!;
 
-    public DbSet<PointOfInterest> PointOfInterests { get; set; } = null!;
+    public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
 
     public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
     {
