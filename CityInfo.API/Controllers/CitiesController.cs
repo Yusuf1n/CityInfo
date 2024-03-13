5ac05d4cebda569a0199fc8d18a23fecdc4fb7c8
Yusuf Naheem <Yusuf.Naheem@vercoglobal.com>
2024-03-13 14:39:21 +0000

    #11.4 - Renaming parameter to make it easier to execute the endpoint in the CLI

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -46,17 +46,17 @@ public class CitiesController : ControllerBase
     /// <summary>
     /// Get a city by id
     /// </summary>
-    /// <param name="id">The id of the city to get</param>
+    /// <param name="cityId">The id of the city to get</param>
     /// <param name="includePointsOfInterest">Whether or not to include the points of interest</param>
     /// <returns>An IActionResult</returns>
     /// <response code="200">Returns the requested city</response>
-    [HttpGet("{id}")]
+    [HttpGet("{cityId}")]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<IActionResult> GetCity(int id, bool includePointsOfInterest = false)
+    public async Task<IActionResult> GetCity(int cityId, bool includePointsOfInterest = false)
     {
-        var city = await _cityInfoRepository.GetCityAsync(id, includePointsOfInterest);
+        var city = await _cityInfoRepository.GetCityAsync(cityId, includePointsOfInterest);
 
         if (city == null)
         {
